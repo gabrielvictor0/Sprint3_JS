c546087d92d78c5d6c91caf13abfca079c70b63f
GabrielVictor0 <bielgvsa@gmail.com>
2023-10-16 10:55:45 -0300

    calculadora finalizada

--- imc-javascript/js/script.js
+++ imc-javascript/js/script.js
@@ -1,13 +1,13 @@
+const arrPessoas = [];
 
+function calcular(e) {
+    e.preventDefault();
 
-function calcular(params) {
-    event.preventDefault();
+    const nome = document.getElementById('nome').value.trim();
+    const altura = parseFloat(document.getElementById('altura').value);
+    const peso = parseFloat(document.getElementById('peso').value);
 
-    let nome = document.getElementById('nome').value.trim();
-    let altura = parseFloat(document.getElementById('altura').value);
-    let peso = parseFloat(document.getElementById('peso').value);
-
-    if(isNaN(altura) || isNaN(peso) || nome.length ==  0 ) {
+    if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
         alert("É necessário preencher os números ou nome corretamente");
         return;
     }
@@ -16,12 +16,28 @@ function calcular(params) {
     const imc = calcularImc(altura, peso);
     const situacao = retornaSituacao(imc);
 
-    console.log(nome);
-    console.log(altura);
-    console.log(peso);
-    console.log(imc);
-    console.log(imc);
-    console.log(situacao);
+    //Modo padrão de lista de objeto
+
+    // const pessoa = {
+    //     nome : `${nome}`,
+    //     altura :`${altura}`,
+    //     peso : `${peso}`,
+    //     imc : `${imc}`,
+    //     situacao : `${situacao}`
+    // };
+
+    //SHORTHAND - funciona quando a propriedade e valor contém o mesmo nome de variável
+    const pessoa = {
+        nome,
+        altura,
+        peso,
+        imc,
+        situacao
+    };
+
+    arrPessoas.push(pessoa);
+
+    listarPessoas();
 }
 
 function calcularImc(altura, peso) {
@@ -33,17 +49,47 @@ function calcularImc(altura, peso) {
 
 function retornaSituacao(imc) {
     //validar o imc
-    if(imc < 18.5) {
+    if (imc < 18.5) {
         return "Magreza Severa";
-    }else if(imc > 18.5 && imc < 24.99) {
+    } else if (imc > 18.5 && imc < 24.99) {
         return "Peso Normal";
-    }else if(imc > 2 && imc < 29.99) {
+    } else if (imc > 2 && imc < 29.99) {
         return "Acima do peso";
-    }else if(imc > 30 && imc < 34.99) {
+    } else if (imc > 30 && imc < 34.99) {
         return "Obesidade I";
-    }else if(imc > 35 && imc < 39.99) {
+    } else if (imc > 35 && imc < 39.99) {
         return "Obesidade II (severa)";
-    }else {
+    } else {
         return "Cuidadoooooooo!!!";
-    } 
+    }
 }
+
+function listarPessoas() {
+
+//     let template = `<tr>
+//     <td>Rubens</td>
+//     <td>1,60</td>
+//     <td>68</td>
+//     <td>10</td>
+//     <td>Peso pena</td>
+// </tr>`;
+
+//      arrPessoas.forEach(pessoa => {
+//          console.log(pessoa.nome, pessoa.altura, pessoa.peso, pessoa.imc.toFixed(2), pessoa.situacao)
+//     });
+
+let template = ""
+
+arrPessoas.forEach((p) => {
+    template += `<tr>
+    <td>${p.nome}</td>
+    <td>${p.altura}</td>
+    <td>${p.peso}</td>
+    <td>${p.imc.toFixed(2)}</td>
+    <td>${p.situacao}</td>
+</tr>`
+});
+
+document.getElementById('cadastro').innerHTML = template;
+
+};
